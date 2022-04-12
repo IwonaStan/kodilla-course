a79fe5fef30c816512c964a0f13a120c5a3ae64c
Iwona Stań <iwona@stan.hub.pl>
2022-04-12 14:40:59 +0200

    Moje testy

---
+++ src/main/java/Trial.java
@@ -0,0 +1,24 @@
+public class Trial {
+    public static void main(String[] args)
+    {
+        int [] names = new int [6];
+
+        int numberOfElements = names.length;
+        System.out.println("Liczba elementów tablicy = " +numberOfElements);
+        System.out.println("Suma elementów:");
+        int sumOfElements = 0;
+
+        for ( int i=0; i<numberOfElements; i++)
+        { names[i] = i+1;
+            if (i<numberOfElements-1)
+            { System.out.print (names[i]+ " + ");
+            }
+            else
+            { System.out.print (names[i]+ " = ");
+            }
+            sumOfElements = sumOfElements + names[i];
+        }
+        System.out.println( +sumOfElements);
+
+    }
+}
