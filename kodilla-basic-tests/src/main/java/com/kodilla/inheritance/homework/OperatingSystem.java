b3dc2169e35bf64972dcf20cb71a55fa4e4e3819
Iwona Stań <iwona@stan.hub.pl>
2022-04-22 21:16:57 +0200

    Dziedziczenie - rozdzial 3.1

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/inheritance/homework/OperatingSystem.java
@@ -0,0 +1,18 @@
+package com.kodilla.inheritance.homework;
+
+public class OperatingSystem {
+
+    public int yearOfPublishment;
+
+    public OperatingSystem (int yearOfPublishment) {
+        this.yearOfPublishment = yearOfPublishment;
+    }
+
+    public void turnOn() {
+        System.out.println("The operating system is turned on.");
+    }
+    public void turnOff() {
+        System.out.println("The operating system is turned off.");
+    }
+    public int getYearOfPublishment() {return yearOfPublishment;}
+}
---
+++ kodilla-basic-tests/src/main/java/com/kodilla/inheritance/homework/Windows8.java
@@ -0,0 +1,13 @@
+package com.kodilla.inheritance.homework;
+
+public class Windows8 extends OperatingSystem {
+
+    public Windows8() {
+        super(2012);
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("For the next time, update your system to Windows10.");
+    }
+}
