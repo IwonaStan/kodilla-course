434ffe4a59b35d2cde46759455fd69b6ee2e00a5
Iwona Stań <iwona@stan.hub.pl>
2022-04-23 23:50:18 +0200

    Dziedziczenie

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/inheritance/Car.java
@@ -0,0 +1,27 @@
+package com.kodilla.inheritance;
+
+public class Car {
+    private int wheels;
+    private int seats;
+
+    public Car(int wheels, int seats) {
+        this.wheels = wheels;
+        this.seats = seats;
+    }
+
+    public void turnOnLights() {
+        System.out.println("Lights were turned on");
+    }
+    public void openDoors() {
+        System.out.println("Opening 4 doors");
+    }
+    public void displayNumberOfSeats() {
+        System.out.println("Number of seats: " + seats);
+    }
+    public int getWheels() {
+        return wheels;
+    }
+    public int getSeats() {
+        return seats;
+    }
+}
