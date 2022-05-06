2438bcbb51ec28f4dfe648266a2c7d1b8a283a55
Iwona Stań <iwona@stan.hub.pl>
2022-05-06 19:23:18 +0200

    zadanie ksiegozbior

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/Homework/Book.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/Homework/Book.java
@@ -1,17 +1,32 @@
 package com.kodilla.collections.adv.immutable.special.Homework;
 
+import java.util.Objects;
+
 public class Book {
 
-    private static String title;
-    private static String author;
+    private String title;
+    private String author;
 
     public Book(String title, String author) {
         this.title = title;
         this.author = author;
     }
 
-   @Override
-   public String toString() {
-       return "Book title:" + title + " Author:" + author + '\n';
-   }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author);
+    }
+
+    @Override
+    public String toString() {
+        return "___\'" + title +"\' author:\'" + author +"\'___";
+    }
+
 }
--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/Homework/BookManager.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/Homework/BookManager.java
@@ -5,17 +5,24 @@ import java.util.List;
 
 public class BookManager {
 
-    public static Book createBook (String title, String author) {
-        List <Book> bookList = new ArrayList<>();
+    static List <Book> bookList = new ArrayList<>();
 
+    public static Book createBook(String title, String author) {
         // to można było jeszcze skrócić bookList.add(new Book(title, author);
         Book book = new Book(title, author);
         bookList.add(book);
 
-        System.out.println(bookList);
+        int i = bookList.size();
 
-        return new Book(title, author);
+        while (i>1) {
+            if (bookList.get(i-2).equals(book)) {
+            System.out.println("Pozycja "+book+" jest już w spisie.");
+            System.out.println(bookList.get(i-2).hashCode()==book.hashCode());
+            }
+            i--;
         }
+        return new Book(title, author);
+    }
 }
 
 
