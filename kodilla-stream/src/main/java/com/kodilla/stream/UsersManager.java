33b6a6a8688d898d7d75117c49fe2afad7b5801e
Iwona Stań <iwona@stan.hub.pl>
2022-05-13 09:43:32 +0200

    Stream

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/UsersManager.java
@@ -0,0 +1,18 @@
+package com.kodilla.stream;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UsersManager {
+    public static void main(String[] args) {
+        List<String> usernames = UsersRepository.getUsersList()   // [1]
+                .stream()
+                .filter(u -> u.getGroup().equals("Chemists"))
+                .map(UsersManager::getUserName)
+                .collect(Collectors.toList());                        // [2]
+        System.out.println(usernames);
+    }
+
+    public static String getUserName(User user) {
+        return user.getUsername();
+    }
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/UsersRepository.java
@@ -0,0 +1,17 @@
+package com.kodilla.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsersRepository {
+    public static List<User> getUsersList() {
+        List<User> users = new ArrayList<>();
+        users.add(new User("Walter White", 50, 7, "Chemists"));
+        users.add(new User("Jessie Pinkman", 25, 4648, "Sales"));
+        users.add(new User("Tuco Salamanca", 34, 116, "Manager"));
+        users.add(new User("Gus Firing", 49, 0, "Board"));
+        users.add(new User("Gale Boetticher", 44, 2, "Chemists"));
+        users.add(new User("Mike Ehrmantraut", 57, 0, "Security"));
+        return users;
+    }
+}
