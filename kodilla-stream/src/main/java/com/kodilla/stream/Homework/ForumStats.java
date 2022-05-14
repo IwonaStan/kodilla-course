b2a82e242bc5eeb1bd316cb47cdbc25114e4bc58
Iwona Stań <iwona@stan.hub.pl>
2022-05-14 10:37:34 +0200

    Strumienie typów liczbowych

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/Homework/ForumStats.java
@@ -0,0 +1,24 @@
+package com.kodilla.stream.Homework;
+
+import com.kodilla.stream.UsersRepository;
+
+public class ForumStats {
+    public static void main(String[] args) {
+
+        double young = UsersRepository.getUsersList()
+                .stream()
+                .filter(a -> a.getAge()<=40)
+                .mapToInt(p -> p.getNumberOfPost())
+                .average()
+                .getAsDouble();
+        System.out.println(young);
+
+        double old = UsersRepository.getUsersList()
+                .stream()
+                .filter(a -> a.getAge()>40)
+                .mapToInt(p -> p.getNumberOfPost())
+                .average()
+                .getAsDouble();
+        System.out.println(old);
+    }
+}
