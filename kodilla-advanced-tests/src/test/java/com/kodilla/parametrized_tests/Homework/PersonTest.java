cc3d2eea2dd88a75fdc306b42e691976cb3c34f4
Iwona Stań <iwona@stan.hub.pl>
2022-05-22 16:00:56 +0200

    Obiekty jako dane wejsciowe

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/Homework/PersonTest.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/Homework/PersonTest.java
@@ -8,20 +8,8 @@ class PersonTest {
 
     @ParameterizedTest
     @MethodSource(value = "com.kodilla.parametrized_tests.Homework.PersonSources#getBMI")
-    void getBMI1(double weight, double height) {
+    void getBMI(double weight, double height) {
         Person person = new Person(weight, height);
         assertEquals("Very severely underweight", person.getBMI());
-    }
-    @ParameterizedTest
-    @MethodSource(value = "com.kodilla.parametrized_tests.Homework.PersonSources#getBMI")
-    void getBMI2(double weight, double height) {
-        Person person = new Person(weight, height);
-        assertEquals("Severely underweight", person.getBMI());
-    }
-    @ParameterizedTest
-    @MethodSource(value = "com.kodilla.parametrized_tests.Homework.PersonSources#getBMI")
-    void getBMI3(double weight, double height) {
-        Person person = new Person(weight, height);
-        assertEquals("Underweight", person.getBMI());
     }
 }
