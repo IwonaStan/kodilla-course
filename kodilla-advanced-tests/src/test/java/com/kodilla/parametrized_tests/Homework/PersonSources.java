9cce773636fc2fb5fdbe3b033e174caf8788d086
Iwona Stań <iwona@stan.hub.pl>
2022-05-23 11:36:44 +0200

    Obiekty jako dane wejsciowe

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/Homework/PersonSources.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/Homework/PersonSources.java
@@ -8,17 +8,17 @@ public class PersonSources {
 
     static Stream<Arguments> getBMI() {
         return Stream.of(
-                Arguments.of(1.7, 40),
-                Arguments.of(1.7, 45),
-                Arguments.of(1.7, 50),
-                Arguments.of(1.7, 55),
-                Arguments.of(1.7, 75),
-                Arguments.of(1.7, 90),
-                Arguments.of(1.7, 105),
-                Arguments.of(1.7, 120),
-                Arguments.of(1.7, 135),
-                Arguments.of(1.7, 150),
-                Arguments.of(1.7, 190)
+                Arguments.of(1.7, 40, "Very severely underweight"),
+                Arguments.of(1.7, 45, "Severely underweight"),
+                Arguments.of(1.7, 50, "Underweight"),
+                Arguments.of(1.7, 55, "Normal (healthy weight)"),
+                Arguments.of(1.7, 75, "Overweight"),
+                Arguments.of(1.7, 90, "Obese Class I (Moderately obese)"),
+                Arguments.of(1.7, 105, "Obese Class II (Severely obese)"),
+                Arguments.of(1.7, 120, "Obese Class III (Very severely obese)"),
+                Arguments.of(1.7, 135, "Obese Class IV (Morbidly Obese)"),
+                Arguments.of(1.7, 150, "Obese Class V (Super Obese)"),
+                Arguments.of(1.7, 190, "Obese Class VI (Hyper Obese)")
         );
     }
 }
--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/Homework/PersonTest.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/Homework/PersonTest.java
@@ -9,68 +9,8 @@ class PersonTest {
 
     @ParameterizedTest
     @MethodSource(value = "com.kodilla.parametrized_tests.Homework.PersonSources#getBMI")
-    void getBMI1(double height, double weight) {
+    void getBMI1(double height, double weight, String expected) {
         Person person = new Person(height, weight);
-        assertEquals("Very severely underweight", person.getBMI());
-    }
-    @ParameterizedTest
-    @MethodSource(value = "com.kodilla.parametrized_tests.Homework.PersonSources#getBMI")
-    void getBMI2(double height, double weight) {
-        Person person = new Person(height, weight);
-        assertEquals("Severely underweight", person.getBMI());
-    }
-    @ParameterizedTest
-    @MethodSource(value = "com.kodilla.parametrized_tests.Homework.PersonSources#getBMI")
-    void getBMI3(double height, double weight) {
-        Person person = new Person(height, weight);
-        assertEquals("Underweight", person.getBMI());
-    }
-    @ParameterizedTest
-    @MethodSource(value = "com.kodilla.parametrized_tests.Homework.PersonSources#getBMI")
-    void getBMI4(double height, double weight) {
-        Person person = new Person(height, weight);
-        assertEquals("Normal (healthy weight)", person.getBMI());
-    }
-    @ParameterizedTest
-    @MethodSource(value = "com.kodilla.parametrized_tests.Homework.PersonSources#getBMI")
-    void getBMI5(double height, double weight) {
-        Person person = new Person(height, weight);
-        assertEquals("Overweight", person.getBMI());
-    }
-    @ParameterizedTest
-    @MethodSource(value = "com.kodilla.parametrized_tests.Homework.PersonSources#getBMI")
-    void getBMI6(double height, double weight) {
-        Person person = new Person(height, weight);
-        assertEquals("Obese Class I (Moderately obese)", person.getBMI());
-    }
-    @ParameterizedTest
-    @MethodSource(value = "com.kodilla.parametrized_tests.Homework.PersonSources#getBMI")
-    void getBMI7(double height, double weight) {
-        Person person = new Person(height, weight);
-        assertEquals("Obese Class II (Severely obese)", person.getBMI());
-    }
-    @ParameterizedTest
-    @MethodSource(value = "com.kodilla.parametrized_tests.Homework.PersonSources#getBMI")
-    void getBMI8(double height, double weight) {
-        Person person = new Person(height, weight);
-        assertEquals("Obese Class III (Very severely obese)", person.getBMI());
-    }
-    @ParameterizedTest
-    @MethodSource(value = "com.kodilla.parametrized_tests.Homework.PersonSources#getBMI")
-    void getBMI9(double height, double weight) {
-        Person person = new Person(height, weight);
-        assertEquals("Obese Class IV (Morbidly Obese)", person.getBMI());
-    }
-    @ParameterizedTest
-    @MethodSource(value = "com.kodilla.parametrized_tests.Homework.PersonSources#getBMI")
-    void getBMI10(double height, double weight) {
-        Person person = new Person(height, weight);
-        assertEquals("Obese Class V (Super Obese)", person.getBMI());
-    }
-    @ParameterizedTest
-    @MethodSource(value = "com.kodilla.parametrized_tests.Homework.PersonSources#getBMI")
-    void getBMI11(double height, double weight) {
-        Person person = new Person(height, weight);
-        assertEquals("Obese Class VI (Hyper Obese)", person.getBMI());
+        assertEquals(person.getBMI(),expected);
     }
 }
