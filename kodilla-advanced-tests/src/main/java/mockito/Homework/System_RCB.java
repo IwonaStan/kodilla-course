ccda986175111be9518202bc9f0dbb63f5622114
Iwona Stań <iwona@stan.hub.pl>
2022-05-28 17:46:08 +0200

    Mockito

--- kodilla-advanced-tests/src/main/java/mockito/Homework/System_RCB.java
+++ kodilla-advanced-tests/src/main/java/mockito/Homework/System_RCB.java
@@ -12,32 +12,33 @@ public class System_RCB {
     public void addNewSubscriber (Subscriber subscriber, Set<Localization> localizations) {
         this.users.put(subscriber, localizations);
     }
-    public void addNewLocalization (Subscriber subscriber, Localization localization) {
+    public void addNewLocalization (Subscriber subscriber, Localization local) {
         Set<Localization> localizations = this.users.get(subscriber);
-        localizations.add(localization);
+        localizations.add(local);
         this.users.put(subscriber, localizations);
     }
     public void sendRegulations (String regulations) {
         for (Map.Entry<Subscriber, Set<Localization>> usersEntry : users.entrySet())
             usersEntry.getKey().sendRegulations(regulations);
     }
-    public void sendAlert (Localization localization, Alert_RCB alert_rcb) {
-        Set<Localization> localizations = new HashSet<>();
+    public void sendAlert (Localization local, Alert_RCB alert_rcb) {
+        Set<Localization> localizations1 = new HashSet<>();
         for (Map.Entry<Subscriber, Set<Localization>> usersEntry : users.entrySet()) {
-            localizations = usersEntry.getValue();
-            for (Localization city : localizations)
-                if (city.equals(localization))
+            localizations1 = usersEntry.getValue();
+            for (Localization city : localizations1)
+                if (city.equals(local))
                     usersEntry.getKey().sendAlert(alert_rcb);
         }
     }
     public void removeSubscriber (Subscriber subscriber) {
         this.users.remove(subscriber);
     }
-    public void removeLocalization (Subscriber subscriber, String localization) {
+    public void removeLocalization (Subscriber subscriber, Localization localization) {
         Set<Localization> user = this.users.get(subscriber);
-        if (user.size()!=0) {
+        if (user.size()>0) {
             user.remove(localization);
             this.users.put(subscriber, user);
-        } else removeSubscriber(subscriber);
+        }
+        if (user.size()==0 )removeSubscriber(subscriber);
     }
 }
--- kodilla-advanced-tests/src/test/java/mockito/Homework/System_RCBTest.java
+++ kodilla-advanced-tests/src/test/java/mockito/Homework/System_RCBTest.java
@@ -1,21 +1,99 @@
 package mockito.Homework;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.util.HashSet;
 import java.util.Set;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class System_RCBTest {
 
     System_RCB system_rcb = new System_RCB();
     Subscriber subscriber = Mockito.mock(Subscriber.class);
-    Localization localizations = Mockito.mock(Localization.class);
+    Localization localization = Mockito.mock(Localization.class);
+    Set<Localization> places = new HashSet<>();
     Alert_RCB alert_rcb = Mockito.mock(Alert_RCB.class);
 
     @Test
     public void addNewSubscriber() {
-        system_rcb.addNewSubscriber(subscriber, system_rcb.addNewLocalization(subscriber,);
+        system_rcb.addNewSubscriber(subscriber, places);
+
+        Assertions.assertEquals(1,system_rcb.users.size());
+    }
+    @Test
+    public void addNewLocalization() {
+        system_rcb.addNewSubscriber(subscriber, places);
+        system_rcb.addNewLocalization(subscriber, localization);
+
+        Assertions.assertEquals(1,system_rcb.users.get(subscriber).size());
+    }
+    @Test
+    public void sendRegulations() {
+        system_rcb.addNewSubscriber(subscriber, places);
+
+        system_rcb.sendRegulations("Regulations");
+
+        Mockito.verify(subscriber, Mockito.times(1)).sendRegulations("Regulations");
+    }
+    @Test
+    public void sendRegulationsToNotSubscriber() {
+        system_rcb.sendRegulations("Regulations");
+
+        Mockito.verify(subscriber, Mockito.never()).sendRegulations("Regulations");
+    }
+    @Test
+    public void sendAlert() {
+        system_rcb.addNewSubscriber(subscriber, places);
+        system_rcb.addNewLocalization(subscriber, localization);
+
+        system_rcb.sendAlert(localization, alert_rcb);
+
+        Mockito.verify(subscriber,Mockito.times(1)).sendAlert(alert_rcb);
+    }
+    @Test
+    public void sendAlertToWrongLocalization() {
+        system_rcb.addNewSubscriber(subscriber, places);
+        system_rcb.addNewLocalization(subscriber, localization);
+
+        Localization localization1 = Mockito.mock(Localization.class);
+        system_rcb.sendAlert(localization1, alert_rcb);
+
+        Mockito.verify(subscriber,Mockito.never()).sendAlert(alert_rcb);
+    }
+    @Test
+    public void removeSubscriber() {
+        system_rcb.addNewSubscriber(subscriber, places);
+
+        system_rcb.removeSubscriber(subscriber);
+
+        system_rcb.sendRegulations("Regulations");
+
+        Mockito.verify(subscriber,Mockito.never()).sendRegulations("Regulations");
+    }
+    @Test
+    public void removeLocalization() {
+        system_rcb.addNewSubscriber(subscriber, places);
+        system_rcb.addNewLocalization(subscriber, localization);
+
+        Localization localization1 = Mockito.mock(Localization.class);
+        system_rcb.addNewLocalization(subscriber, localization1);
+
+        system_rcb.removeLocalization(subscriber, localization1);
+
+        Assertions.assertEquals(1,system_rcb.users.get(subscriber).size());
+    }
+    @Test
+    public void removeAllLocalization() {
+        system_rcb.addNewSubscriber(subscriber, places);
+        system_rcb.addNewLocalization(subscriber, localization);
+
+        Localization localization1 = Mockito.mock(Localization.class);
+        system_rcb.addNewLocalization(subscriber, localization1);
+
+        system_rcb.removeLocalization(subscriber, localization);
+        system_rcb.removeLocalization(subscriber, localization1);
+
+        Assertions.assertEquals(0,system_rcb.users.size());
     }
 }
