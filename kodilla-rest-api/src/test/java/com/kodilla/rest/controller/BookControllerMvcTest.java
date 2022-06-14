a872a19b88349361125f467da94f5ecbe9d4f5a9
Iwona Stań <iwona@stan.hub.pl>
2022-06-14 13:38:55 +0200

    Spring REST API

---
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerMvcTest.java
@@ -0,0 +1,46 @@
+package com.kodilla.rest.controller;
+
+import com.kodilla.rest.domain.BookDto;
+import com.kodilla.rest.service.BookService;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(BookController.class)
+public class BookControllerMvcTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookService bookService;
+    private BookController bookController;
+
+    @Test
+    void shouldFetchBooks() throws Exception {
+        //given
+        List<BookDto> booksList = new ArrayList<>();
+        booksList.add(new BookDto("title 1", "author 1"));
+        booksList.add(new BookDto("title 2", "author 2"));
+        Mockito.when(bookService.getBooks()).thenReturn(booksList);
+
+        //when & then
+        mockMvc.perform(MockMvcRequestBuilders.get("/books")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is(200))
+                .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(2)));
+    }
+}
---
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerTest.java
@@ -0,0 +1,41 @@
+package com.kodilla.rest.controller;
+
+import com.kodilla.rest.domain.BookDto;
+import com.kodilla.rest.service.BookService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BookControllerTest {
+
+    @Test
+    public void shouldFetchBooks() {
+        //given
+        BookService bookServiceMock = Mockito.mock(BookService.class);
+        BookController bookController = new BookController(bookServiceMock);
+        List<BookDto> booksList = new ArrayList<>();
+        booksList.add(new BookDto("Title 1", "Author 1"));
+        booksList.add(new BookDto("Title 2", "Author 2"));
+        Mockito.when(bookServiceMock.getBooks()).thenReturn(booksList);
+
+        //when
+        List<BookDto> result = bookController.getBooks();
+
+        //then
+        assertThat(result).hasSize(2);
+    }
+    @Test
+    public void shouldAddBook() {
+        BookService bookService = new BookService();
+        BookController bookController = new BookController(bookService);
+
+        bookController.addBook(new BookDto("Title 1", "Author 1"));
+        List<BookDto> result = bookController.getBooks();
+
+        assertThat(result).hasSize(1);
+    }
+}
