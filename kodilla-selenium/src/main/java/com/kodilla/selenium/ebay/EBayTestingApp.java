b7078d03a20699b580ca64dfb9fc3d8d3fd736f6
Iwona Stań <iwona@stan.hub.pl>
2022-06-29 11:37:00 +0200

    Selenium

--- kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EBayTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EBayTestingApp.java
@@ -8,11 +8,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class EBayTestingApp {
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver", "c:\\Selenium-drivers\\Chrome\\chromedriver.exe");      // [1]
+        System.setProperty("webdriver.chrome.driver", "c:\\Selenium-drivers\\Chrome\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.ebay.com/");
 
-        WebElement inputField = driver.findElement(By.name("_nkw")); // [4]
+        WebElement inputField = driver.findElement(By.name("_nkw"));
         inputField.sendKeys("Laptop");
         inputField.submit();
     }
--- kodilla-selenium/src/main/java/com/kodilla/selenium/google/GoogleExample.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/google/GoogleExample.java
@@ -7,11 +7,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GoogleExample {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "c:\\Selenium-drivers\\Chrome\\chromedriver.exe");      // [1]
+        System.setProperty("webdriver.chrome.driver", "c:\\Selenium-drivers\\Chrome\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
 
-        WebElement inputField = driver.findElement(By.name("q")); // [4]
+        WebElement inputField = driver.findElement(By.name("q"));
         inputField.sendKeys("Kodilla");
         inputField.submit();
     }
